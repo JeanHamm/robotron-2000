e7ef7103e8f250ad0e00c63518d85e7a98db3339
Jean Hamm <jeanmichehamm@gmail.com>
2023-04-17 12:03:34 -0300

    Subindo base v1

---
+++ Testes/main.js
@@ -0,0 +1,6 @@
+const escritas = document.querySelectorAll('.resultado');
+const botao = document.querySelector('#calcular');
+
+escritas[0].innerText=”Fui clicado”;
+
+
---
+++ main.js
@@ -0,0 +1,14 @@
+const robotron = document.querySelector('#robotron');
+
+robotron.addEventListener('click', () => {
+    dizer("pedro");
+    
+});
+
+
+
+function dizer(nome){
+console.log('Oi ' + nome);
+console.log('Seja bem-vindo');
+}
+
